df2a045c6e7d0b3192655d7467e81efdb2698548
DoctorRSS <silver.rob16@yahoo.com.br>
2020-05-12 21:56:03 -0300

    ajuste na Url de producao

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
  // apiUrl: 'https://algamoney-api.herokuapp.com'
-  apiUrl: 'https://algamoney-ui-robson-silveira.herokuapp.com/'
+  apiUrl: 'https://algamoney-ui-robson-silveira.herokuapp.com'
 };
